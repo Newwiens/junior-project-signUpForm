f643004fb0962645feb570c2fdb62825cf8f319b
Newwiens <ht.nguyen013@gmail.com>
2025-09-06 13:40:07 +0200

    update

---
+++ js/formValidation.js
@@ -0,0 +1,54 @@
+// submit
+const form = document.querySelector(".signup-form");
+
+// variable aanmaken
+const firstname = document.querySelector("#firstname");
+const firstnameError = document.getElementById(
+  firstname.getAttribute("aria-describedby")
+);
+
+const lastname = document.querySelector("#lastname");
+const lastnameError = document.getElementById(
+  lastname.getAttribute("aria-describedby")
+);
+
+//let valid = true;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  firstname.removeAttribute("aria-invalid"); // veld is weer geldig
+  lastname.removeAttribute("aria-invalid");
+  firstnameError.textContent = "";
+  lastnameError.textContent = "";
+
+  //   if check empty strin
+  if (firstname.value.trim() === "") {
+    firstname.setAttribute("aria-invalid", "true"); // rood maken in CSS
+    firstnameError.textContent = "First name cannot be empty";
+  }
+
+  if (lastname.value.trim() == "") {
+    lastname.setAttribute("aria-invalid", "true");
+    lastnameError.textContent = "Last name cannot be empty";
+  }
+
+  console.log("BTN is geklikt");
+});
+
+const inputs = [
+  {
+    inputId: firstname,
+    error: firstnameError,
+  },
+  { inputId: lastname, error: lastnameError },
+];
+
+inputs.forEach(({ inputId, error }) => {
+  inputId.addEventListener("input", () => {
+    if (inputId.value.trim() !== "") {
+      inputId.removeAttribute("aria-invalid");
+      error.textContent = "";
+    }
+  });
+});
