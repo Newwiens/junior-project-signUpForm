7233aaba6957478a70f3921185607dde64938919
Newwiens <ht.nguyen013@gmail.com>
2025-09-08 23:21:21 +0200

    update

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,7 +1,12 @@
-// submit
+// --- Global variable ---
 const form = document.querySelector(".signup-form");
 
-// variable aanmaken
+// name@domein.anyTld
+const EMAIL_REGEX = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i;
+// Minstens 8 tekens, 1 cijfer, 1 speciaal teken
+const PASSWORD_REGEX = /^(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,}$/;
+
+// --- Elements & error targets ---
 const firstname = document.querySelector("#firstname");
 const firstnameError = document.getElementById(
   firstname.getAttribute("aria-describedby")
@@ -12,10 +17,20 @@ const lastnameError = document.getElementById(
   lastname.getAttribute("aria-describedby")
 );
 
-const email = document.querySelector("#email")
-const emailError = document.getElementById(email.getAttribute("aria-describedby"));
+const email = document.querySelector("#email");
+const emailError = document.getElementById(
+  email.getAttribute("aria-describedby")
+);
 
-//let valid = true;
+const password = document.querySelector("#password");
+const passwordError = document.getElementById(
+  password.getAttribute("aria-describedby")
+);
+
+const passConfirm = document.querySelector("#passConfirm");
+const passConfirmError = document.getElementById(
+  passConfirm.getAttribute("aria-describedby")
+);
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -23,32 +38,60 @@ form.addEventListener("submit", (e) => {
   firstname.removeAttribute("aria-invalid"); // veld is weer geldig
   lastname.removeAttribute("aria-invalid");
   email.removeAttribute("aria-invalid");
+  password.removeAttribute("aria-invalid");
+  passConfirm.removeAttribute("aria-invalid");
 
   firstnameError.textContent = "";
   lastnameError.textContent = "";
   emailError.textContent = "";
+  passwordError.textContent = "";
+  passConfirmError.textContent = "";
 
   //   Check First Name
   if (firstname.value.trim() === "") {
     firstname.setAttribute("aria-invalid", "true"); // rood maken in CSS
     firstnameError.textContent = "First name cannot be empty";
-  }else if (firstname.value.trim().length < 2) {
-    firstname.setAttribute("aria-invalid",  "true");
+  } else if (firstname.value.trim().length < 2) {
+    firstname.setAttribute("aria-invalid", "true");
     firstnameError.textContent = "First name must be at least 2 characters";
   }
 
   //Check Last Name
   if (lastname.value.trim() === "") {
-  lastname.setAttribute("aria-invalid", "true");
-  lastnameError.textContent = "Last name cannot be empty";
-} else if (lastname.value.trim().length < 2) {
-  lastname.setAttribute("aria-invalid", "true");
-  lastnameError.textContent = "Last name must be at least 2 characters";
+    lastname.setAttribute("aria-invalid", "true");
+    lastnameError.textContent = "Last name cannot be empty";
+  } else if (lastname.value.trim().length < 2) {
+    lastname.setAttribute("aria-invalid", "true");
+    lastnameError.textContent = "Last name must be at least 2 characters";
   }
 
+  //Check Email
   if (email.value.trim() === "") {
     email.setAttribute("aria-invalid", "true");
-    emailError.textContent = "Please enter a valid email address"
+    emailError.textContent = "Please enter a valid email address";
+  } else if (!email.value.includes("@")) {
+    email.setAttribute("aria-invalid", "true");
+    emailError.textContent = "Email address must contain '@' and a domain";
+  } else if (email.value.trim().endsWith("@")) {
+    email.setAttribute("aria-invalid", "true");
+    emailError.textContent = "Email address must contain a domain name";
+  } else if (!EMAIL_REGEX.test(email.value.trim())) {
+    email.setAttribute("aria-invalid", "true");
+    emailError.textContent = "Please enter a valid email address";
+  }
+
+  if (password.value.trim() === "") {
+    password.setAttribute("aria-invalid", "true");
+    passwordError.textContent = "Password cannot be empty";
+  } else if (password.value.trim().length < 8) {
+    password.setAttribute("aria-invalid", true);
+    passwordError.textContent = "Password must be at least 8 characters";
+  } else if (!PASSWORD_REGEX.test(password.value.trim())) {
+    password.setAttribute("aria-invalid", "true");
+    passwordError.textContent =
+      "Password must be at least 8 characters,\n" +
+      "at least 1 number,\n" +
+      "and at least 1 special character";
   }
 
   console.log("BTN is geklikt");
@@ -60,7 +103,8 @@ const inputs = [
     error: firstnameError,
   },
   { inputId: lastname, error: lastnameError },
-  { inputId: email, error: emailError},
+  { inputId: email, error: emailError },
+  { inputId: password, error: passwordError },
 ];
 
 inputs.forEach(({ inputId, error }) => {
